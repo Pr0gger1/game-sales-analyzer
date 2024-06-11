e4a90075eabe417324ea5a45b9830f8abfb917aa
Pr0gger1 <pr0gger.dev@gmail.com>
2024-06-11 20:36:53 +0300

    исправлена ошибка в выводе графиков продаж игры по регионам, когда графиков более 4

--- scripts/analysis_by_regions/game_by_regions.py
+++ scripts/analysis_by_regions/game_by_regions.py
@@ -1,13 +1,14 @@
+import math
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as f
 
 import matplotlib.pyplot as plt
 from pandas import DataFrame as PDataFrame
 
+from utils.file_utils import save_plot_as_image
 from utils.spark_utils import column_dict as cd
 
-
-def get_game_sales_by_regions(df: DataFrame, game_name: str) -> DataFrame:
+def get_game_sales_by_regions(df: DataFrame, game_name: str, export_as_img: bool = True) -> DataFrame:
     title: str = cd["title"]
     filtered_df: DataFrame = df.filter(f.lower(f.col(title)).contains(game_name.lower()))
     grouped_df: DataFrame = filtered_df.groupBy([title]).agg(
@@ -18,31 +19,41 @@ def get_game_sales_by_regions(df: DataFrame, game_name: str) -> DataFrame:
     )
      
     grouped_df.show(truncate=False)
-    visualize_game_sales_by_regions(grouped_df)
+    visualize_game_sales_by_regions(grouped_df, game_name, export_as_img)
     
     return grouped_df
     
-def visualize_game_sales_by_regions(df: DataFrame):
+def visualize_game_sales_by_regions(df: DataFrame, game_name: str, export_as_img: bool = True):
     pdf: PDataFrame = df.na.drop().toPandas()
     regions: list[str] = [col for col in df.columns if col.endswith("sales")]
     game_list: list[str] = pdf.iloc[:, 0].values
-    plt.figure(figsize=(12, 8))
-    plt.suptitle("Распределение продаж игры по регионам")
     
-    for it in range(len(pdf)):
-        sales = pdf.iloc[it, 1:].values.astype(float)
-        plt.subplot(2, 2, it + 1)
-        plt.bar(
-            regions,
-            sales,
-            color=['blue', 'green', 'red', 'purple'],
-            width=0.5
-        )
-        plt.xlabel("Регионы")
-        plt.ylabel("Объем продаж (млн)")
-        plt.title(f"{game_list[it]}")
-        
-    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1, hspace=0.4, wspace=0.4)
-        
-    plt.show()
+    num_games = len(game_list)
+    cols = 2
+    rows = 2
+    games_per_figure = rows * cols
     
+    for fig_num in range(math.ceil(num_games / games_per_figure)):
+        plt.figure(figsize=(12, 8))
+        plt.suptitle("Распределение продаж игры по регионам")
+        
+        for it in range(games_per_figure):
+            index = fig_num * games_per_figure + it
+            if index >= num_games:
+                break
+            sales = pdf.iloc[index, 1:].values.astype(float)
+            plt.subplot(rows, cols, it + 1)
+            plt.bar(
+                regions,
+                sales,
+                color=['blue', 'green', 'red', 'purple'],
+                width=0.5
+            )
+            plt.xlabel("Регионы")
+            plt.ylabel("Объем продаж (млн)")
+            plt.title(f"{game_list[index]}")
+            if export_as_img:
+                save_plot_as_image(plt.gcf(), f"{game_name}_{it + 1}_sales_by_regions.jpg")
+        
+        plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1, hspace=0.4, wspace=0.4)
+        plt.show()
--- scripts/analysis_by_regions/game_sales_estimates.py
+++ scripts/analysis_by_regions/game_sales_estimates.py
@@ -1,12 +1,14 @@
 from utils.spark_utils import column_dict as cd
-import seaborn as sns
 from utils.file_utils import save_plot_as_image
+
+import seaborn as sns
 import matplotlib.pyplot as plt
+
 from pandas.core.frame import DataFrame as PDataFrame
 from pyspark.sql import DataFrame
 
 
-def get_game_sale_estimates(df: DataFrame, as_file: bool = True):
+def get_game_sale_estimates(df: DataFrame, export_as_img: bool = True):
     df_clean = df.na.drop(subset=[
         cd['critic_score'],
         cd['na_sales'],
@@ -36,11 +38,11 @@ def get_game_sale_estimates(df: DataFrame, as_file: bool = True):
 
     # Преобразование в Pandas DataFrame
     pandas_df: PDataFrame = df_clean.toPandas()
-    build_sales_distribution_by_critic_score_plot(pandas_df, as_file)
-    build_heatmap_correlation_matrix(pandas_df, sales_columns, as_file)
+    build_sales_distribution_by_critic_score_plot(pandas_df, export_as_img)
+    build_heatmap_correlation_matrix(pandas_df, sales_columns, export_as_img)
 
 
-def build_sales_distribution_by_critic_score_plot(df: PDataFrame, as_file: bool = True):
+def build_sales_distribution_by_critic_score_plot(df: PDataFrame, export_as_img: bool = True):
     # Визуализация с использованием Seaborn
     sns.set(style="whitegrid")
 
@@ -50,18 +52,18 @@ def build_sales_distribution_by_critic_score_plot(df: PDataFrame, as_file: bool 
     plt.title('Distribution of Critic Scores')
     plt.xlabel('Critic Score')
     plt.ylabel('Frequency')
-    if as_file:
+    if export_as_img:
         save_plot_as_image(plt.gcf(), "critic_scores_distribution.jpg")
     plt.show()
 
 
-def build_heatmap_correlation_matrix(df: PDataFrame, sales_columns: list[str], as_file: bool = True):
+def build_heatmap_correlation_matrix(df: PDataFrame, sales_columns: list[str], export_as_img: bool = True):
     plt.figure(figsize=(10, 6))
     corr_matrix = df[[cd['critic_score']] + sales_columns].corr()
     sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', center=0)
     plt.title('Game Sales Correlation Matrix')
 
-    if as_file:
+    if export_as_img:
         save_plot_as_image(plt.gcf(), "game_sales_correlation.jpg")
 
     plt.show()
